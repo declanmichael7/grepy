b1612bd195d0e5adf211aeaf073d993b99014b2c
declanmichael7 <declanmichael7@gmail.com>
2019-05-11 22:15:56 -0400

    Fixed union issue
    Fixed an issue where union would'n't properly split if it was in parenthesis. Still have the issue of too many accept states

--- NFA.java
+++ NFA.java
@@ -13,12 +13,10 @@ public class NFA{
 			System.out.println("Bad regex");
 		}
 		else{
-			regex = regex.substring(0,regex.length()-1);
+			regex = regex.substring(0,regex.indexOf('$'));
 		}
 		states.add(0);
-		acceptStates.add(0);
 		int currentState = 0;
-		int nextState = 1;
 		Stack<Integer> subExpStart = new Stack<>();
 		int depth = 0;
 		int i = 0;
@@ -37,23 +35,29 @@ public class NFA{
 		}
 		if(unionFlag == true){
 			subExpStart.push(currentState);
-			states.add(nextState);
-			transition = new Transition(currentState, nextState, 'e');
+			states.add(currentState + 1);
+			transition = new Transition(currentState, (currentState + 1), '!');
 		    transitions.add(transition);
 			currentState++;
-			nextState++;
 		}
 		while(pos<regex.length()){
 			if(Character.isLetterOrDigit(regex.charAt(pos))){
-				states.add(nextState);
-				transition = new Transition(currentState,nextState,regex.charAt(pos));
+				states.add(currentState + 1);
+				transition = new Transition(currentState,(currentState + 1),regex.charAt(pos));
 				transitions.add(transition);
 				currentState++;
-				nextState++;
-				acceptStates.set(0, currentState);
+				if(acceptStates.size() == 0){
+					acceptStates.add(currentState);
+				}
+				else{
+					acceptStates.set(0, currentState);
+				}
 			}
 			else if(regex.charAt(pos) == '('){
 				subExpStart.push(currentState);
+				unionFlag = false;
+				depth = -1;
+				i=0;
 				while(i<regex.length()){
 					if(regex.charAt(i) == '+' && depth == 0){
 						unionFlag = true;
@@ -67,19 +71,18 @@ public class NFA{
 					i++;
 				}
 				if(unionFlag == true){
-					states.add(nextState);
-					transition = new Transition(currentState, nextState, 'e');
+					states.add(currentState + 1);
+					transition = new Transition(currentState, (currentState + 1), '!');
 					transitions.add(transition);
 					currentState++;
-					nextState++;
 				}
 			}
 			else if(regex.charAt(pos) == ')'){
 				if(pos+1<regex.length()){
 					if(regex.charAt(pos+1) == '*'){
-						transition = new Transition(currentState, subExpStart.peek(), 'e');
+						transition = new Transition(currentState, subExpStart.peek(), '!');
 						transitions.add(transition);
-						transition = new Transition(subExpStart.peek(), currentState, 'e');
+						transition = new Transition(subExpStart.peek(), currentState, '!');
 						transitions.add(transition);
 						pos++;
 					}
@@ -87,22 +90,24 @@ public class NFA{
 				}
 			}
 			else if(regex.charAt(pos) == '*' && Character.isLetterOrDigit(regex.charAt(pos-1))){
-					transition = new Transition(currentState, currentState-1, 'e');
+					transition = new Transition(currentState, currentState-1, '!');
 					transitions.add(transition);
-					transition = new Transition(currentState-1, currentState, 'e');
+					transition = new Transition(currentState-1, currentState, '!');
 					transitions.add(transition);
 			}
 			else if(regex.charAt(pos) == '+'){
-				states.add(nextState);
-				transition = new Transition(subExpStart.peek(), nextState, 'e');
+				states.add((currentState + 1));
+				transition = new Transition(subExpStart.peek(), (currentState + 1), '!');
 				transitions.add(transition);
 				if(subExpStart.size()==1){
 					acceptStates.add(currentState);
 				}
 				currentState++;
-				nextState++;
 			}
 		    pos++;
+		}
+		if(acceptStates.size()==0){
+			acceptStates.add(0);
 		}
 		System.out.println(states);
 		System.out.println(acceptStates);
